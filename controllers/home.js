909564cd37416a86ed16c4518c826b0c63d9427b
Laura Sims <laurasimsdev@gmail.com>
2022-10-25 21:42:42 -0600

    Updated API

--- controllers/home.js
+++ controllers/home.js
@@ -4,11 +4,12 @@ module.exports = {
     const randomNum = Math.floor(Math.random() * 111);
     const response = await axios
       .get(
-        "https://goquotes-api.herokuapp.com/api/v1/all?type=tag&val=motivational"
+        "https://zenquotes.io/api/random"
       )
+      .catch(error => console.log(error))
       .then(function (response) {
-        quote = response.data.quotes[randomNum].text;
-        author = response.data.quotes[randomNum].author;
+        quote = response.data[0].q;
+        author = response.data[0].a;
       });
 
     res.render("index.ejs", {
@@ -17,3 +18,23 @@ module.exports = {
     });
   },
 };
+
+// const axios = require("axios");
+// module.exports = {
+//   getIndex: async (req, res) => {
+//     const randomNum = Math.floor(Math.random() * 111);
+//     const response = await axios
+//       .get(
+//         "https://goquotes-api.herokuapp.com/api/v1/all?type=tag&val=motivational"
+//       )
+//       .then(function (response) {
+//         quote = response.data.quotes[randomNum].text;
+//         author = response.data.quotes[randomNum].author;
+//       });
+
+//     res.render("index.ejs", {
+//       quote: quote,
+//       author: author,
+//     });
+//   },
+// };
